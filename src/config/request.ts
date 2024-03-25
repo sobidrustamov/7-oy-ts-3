e6ea58253e0c1ab4121e6d2e70a4bc57c72ac538
Sobid <rustamovsobid95@gmail.com>
2024-03-25 13:46:44 +0500

    initial commit

---
+++ src/config/request.ts
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export const request = axios.create({ baseURL: "135.181.108.207/" });
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Button, Form, type FormProps, Input } from "antd";
+import "./style.scss";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+};
+
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+const handleLogin = () => {};
+
+const Login: React.FC = () => (
+  <div className="login">
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      onSubmitCapture={handleLogin}
+    >
+      <Form.Item<FieldType>
+        label="Username"
+        name="username"
+        onMetaChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  </div>
+);
+
+export default Login;
